e86c86f1f2af52b95a044a6ef727855b3cfe8a49
MartfAe <anaemilia@aluno.ufrb.edu.br>
2023-05-22 09:33:02 -0300

    Código POO subindo

---
+++ banco_dados/src/main/java/br/ufrb/edu/gcet236/banco_dados/BancoDadosApplication.java
@@ -0,0 +1,12 @@
+package br.ufrb.edu.gcet236.banco_dados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDadosApplication.class, args);
+	}
+}
---
+++ banco_dados/src/test/java/br/ufrb/edu/gcet236/banco_dados/BancoDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrb.edu.gcet236.banco_dados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
